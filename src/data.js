af442d9b6bea8a09207ff28dda1f23e3ca122f8e
pamolowo <pam.olowo@gmail.com>
2023-07-19 22:29:08 +0100

    my portfolio

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+    {
+      title: "Teeket",
+      subtitle: "React with Firebase",
+      description: "Agroup project presented by group 1, cohort-6 of HerTechTrail",
+      image: require("./Assets/Teeket.png"),
+      link: "https://teeket.netlify.app/",
+    },
+    {
+      title: "Mealy",
+      subtitle: "React",
+      description: "A Team project presented by Team 4a , cohort 1.4 of STUTERN graduate accelerator program",
+      image: require ("./Assets/Mealy.png"),
+      link: "https://mealy4u.netlify.app/",
+    },
+    
+    // {
+    //   title: " Todo App",
+    //   subtitle: "Javascript",
+    //   description:
+    //     "",
+    //   image: "./Assets/Teeket.png",
+    //   link: "https://reactbootcamp.com",
+    // },
+
+  ];
+
+  export const skills = [
+    "Design",
+    "Animation",
+    "JavaScript",
+    "React",
+    "Devops",
+    "Python"
+  ];
+  
